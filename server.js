76c3fcf3e18a329a3e31d0e3a9683e59cd3ca368
Michelle Gaffney <mgaff87@gmail.com>
2019-02-21 17:21:17 -0500

    set up CORS

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
- const app = express();
+const app = express();
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
 
- const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
- app.get('/api/*', (req, res) => {
-   res.json({ok: true});
- });
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
- app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.get('/api/*', (req, res) => {
+    res.json({ok: true});
+});
 
- module.exports = {app};
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = {app};
